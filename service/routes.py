87de384ef8e6acb075410d91b0e89e195d9f0f2e
AYAGALLAL <ayagallal16@gmail.com>
2025-04-07 19:25:00 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -102,9 +102,16 @@ def create_products():
 # PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
 #
 
-######################################################################
-# R E A D   A   P R O D U C T
-######################################################################
+
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+        
 
 #
 # PLACE YOUR CODE HERE TO READ A PRODUCT
